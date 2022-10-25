2b9d1a45079414bd8b8fbbb22de4b773fdee1dfe
Katie Campbell <katherine.campbell2@emory.edu>
2022-10-25 13:12:54 -0400

    Katie made changes to the 01_code and included her final figure and description in the RMD report

--- code/01_Katie.R
+++ code/01_Katie.R
@@ -2,13 +2,31 @@ here::i_am(
   "code/01_Katie.R"
 )
 
+# Call in the clean data
 engagement <- readRDS(
   file = here::here("output/data_clean.rds")
 )
 
 ####
+# Create a summarized data set with average views per week
+figure1 <- engagement %>% group_by(week) %>% summarise(average_views = mean(views), sd = sd(views))
 
-# Analysis here to create a table or figure 
+library(ggplot2)
+
+fig1 <- ggplot(figure1, aes(x=week, y=average_views)) +
+  geom_bar(position=position_dodge(), stat="identity",
+           colour='black', fill = "#0099f9") +
+  geom_errorbar(aes(ymin=average_views-sd, ymax=average_views+sd), width=.2) +
+  theme_classic()+
+  labs(
+    title = "Figure 1. Engagement with pre-read videos by week",
+    x = "Course week",
+    y = "Average number of views"
+  )
+
+
+
+
 # call the figure or table you create katie1, katie2, etc.
 
 ####
